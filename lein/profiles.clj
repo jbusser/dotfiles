7836dbcd2e2638bd0d94b41ff173c4720d9f4faa
Jerry Busser <jerry.busser@gmail.com>
2016-03-27 11:00:46 -0400

    Add lein-auto to profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,4 @@
-{:user {:plugins [[lein-exec  "0.3.4"] [cider/cider-nrepl "0.10.2"]]}}
+{:user {:plugins [
+                  [lein-exec "0.3.4"]
+                  [cider/cider-nrepl "0.10.2"]
+                  [lein-auto "0.1.2"]]}}
